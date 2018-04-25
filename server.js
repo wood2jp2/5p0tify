a11ef9422a02dfcc6798c3f99605540ed7831d04
Josh Wood <wood2jp2@gmail.com>
2018-04-25 13:23:50 -0400

    trying to send data back to front end...

--- server.js
+++ server.js
@@ -5,36 +5,39 @@ const express = require('express'),
       id: '96bc7054fa6f4fcfb7fcc1d4641e94b0',
       secret: '8d71e97def8e410499e5cc102f192351'
     }),
+    hey = {
+      something: 'fdsasdf',
+      finalGenres: []
+    },
     port = process.env.PORT || 5000;
 
 app.get('/api/hey', (req, res) => {
-
+  
   spotify.request('https://api.spotify.com/v1/users/1276640268/playlists/2kpoUUJ5a4Cw3feTkFJhZ2')
       .then( data => {
-        let artistId = data.tracks.items[32].track.album.artists[0].id
-          spotify.request(`https://api.spotify.com/v1/artists/${artistId}`)
-            .then( res => {
-            console.log(res.genres[0])
-              })
-            .catch(error => {
-            console.log('ERROR: ' + error)
-              })
 
+        let songArray = data.tracks.items
+
+        let artistIdArray = songArray.map(song => song.track.album.artists[0].id);
+
+        for (let i = 0; i < artistIdArray.length; i++) {
+
+            spotify.request(`https://api.spotify.com/v1/artists/${artistIdArray[i]}`)
+            .then( resp => {
+              hey.finalGenres.push(resp.genres[0])
+              
+            })
+            .catch(error => {
+                console.log('ERROR: ' + error)
+            })
+
+           
+        }
       })
       .catch(err => {
         console.log('ERROR: ' + err)
       })
-
-  // spotify.request('https://api.spotify.com/v1/artists/7n2wHs1TKAczGzO7Dd2rGr')
-  //     .then( data => {
-  //       console.log(data)
-  //     })
-  //     .catch(err => {
-  //       console.log('ERROR: ' + err)
-  //     })
-   
-
-  res.send({ express: 'Hello From kokoko' });
+      .then(res.send({ express: hey.finalGenres }))
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
